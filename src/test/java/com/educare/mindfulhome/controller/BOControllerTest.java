7557d603627cfb8aaab1df1db1c5cff3374a8c0f
sgad <shani_gad@intuit.com>
2022-01-30 18:57:05 +0200

    Add BO controller validation test

---
+++ src/test/java/com/educare/mindfulhome/controller/BOControllerTest.java
@@ -0,0 +1,32 @@
+package com.educare.mindfulhome.controller;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.educare.mindfulhome.controller.payload.CreateActivityRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(controllers = BOController.class)
+class BOControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  @Test
+  void whenEmptyName_thenReturns400() throws Exception {
+    CreateActivityRequest createActivityRequest = new CreateActivityRequest("", "www");
+
+    mockMvc.perform(post("/bo/activity")
+            .contentType("application/json")
+            .content(objectMapper.writeValueAsString(createActivityRequest)))
+        .andExpect(status().isBadRequest());
+  }
+
+}
